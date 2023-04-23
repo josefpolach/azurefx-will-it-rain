24e57221aabc807b7bf43f61809149ba3c6f0c27
Josef Polach <9721300+josefpolach@users.noreply.github.com>
2023-04-23 16:30:51 +0200

    requirements.txt update

--- Will-it-rain-time-trigger/__init__.py
+++ Will-it-rain-time-trigger/__init__.py
@@ -1,14 +1,70 @@
 import datetime
 import logging
+import sys
+import requests
+import os
+from twilio.rest import Client
+from dotenv import load_dotenv
+
 
 import azure.functions as func
 
 
 def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().replace(
-        tzinfo=datetime.timezone.utc).isoformat()
+   # load configuration from .env file
+    load_dotenv()
 
-    if mytimer.past_due:
-        logging.info('The timer is past due!')
+    # configure LOGGING
+    logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s",
+                        level= logging.INFO)
 
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+    # Create a Twilio account to get these secrets
+    TWILLIO_ACCOUNT_SID = os.environ.get('TWILLIO-ACCOUNT-SID')
+    TWILLIO_AUTH_TOKEN = os.environ.get('TWILLIO-AUTH-TOKEN')
+    TWILLIO_MOBILE_NUMBER = os.environ.get('TWILLIO-MOBIL-NUMBER')
+    API_OPENWEATHER_KEY = os.environ.get('OW-API')
+    YOUR_MOBIL_NUMBER = os.environ.get('YOUR-MOBIL-NUMBER')
+
+    
+
+    COORDINATES = {
+        "praha": (50.087811, 14.420460),    # Prague, Czech Republic
+        'neuried': (48.093079, 11.465850)   # Neuried, Germany
+    }
+
+    API_URL_ONE_CALL = 'https://api.openweathermap.org/data/2.5/onecall'
+
+    params = {
+        'lat': COORDINATES['praha'][0],
+        'lon': COORDINATES['praha'][1],
+        'appid': API_OPENWEATHER_KEY,
+        'exclude': 'current,minutely,daily',
+        'lang': 'cz',
+    }
+
+    try:
+        response = requests.get(url=API_URL_ONE_CALL, params=params)
+        response.raise_for_status()
+        logging.info(f'Open Weather Request Status: {response.reason}')
+    except requests.exceptions.HTTPError as e:
+        logging.error(f"HTTP error occurred: {e.response.content.decode('utf-8')}")
+        sys.exit("Fatal Error")
+    else:
+        data = response.json()['hourly']
+        will_rain = False
+
+        for hour in data[slice(12)]:
+            if 900 > hour['weather'][0]['id']:
+                will_rain = True
+                logging.info('OW: It will be raining today. An SMS will be send.')
+            else:
+                logging.info('OW: TODAY will not be raining. End of program...\n')
+        if will_rain:
+            client = Client(TWILLIO_ACCOUNT_SID, TWILLIO_AUTH_TOKEN)
+            message = client.messages \
+                .create(
+                body="It's going to rain. Remember to bring an ☂️",
+                from_=TWILLIO_MOBILE_NUMBER,
+                to= YOUR_MOBIL_NUMBER
+            )
+            logging.info(f"SMS was sent '{message.status}'. End of program...\n")
